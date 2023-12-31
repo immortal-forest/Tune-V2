71dec41031fcbd107ccdd609ba469d3cda45564f
immortal-forest <ali.salman.sa.01@gmail.com>
2023-12-31 17:14:54 +0545

    AAAAAWAIT

--- src/database/music/utils/database_checks.py
+++ src/database/music/utils/database_checks.py
@@ -2,7 +2,7 @@ import asyncpg
 
 
 async def check_database(dsn: str, dbname: str):
-    async with asyncpg.connect(dsn) as conn:
+    async with await asyncpg.connect(dsn) as conn:
         exists = await conn.fetchval(
             "SELECT 1 FROM pg_database WHERE datname = $1;",
             dbname
@@ -11,7 +11,7 @@ async def check_database(dsn: str, dbname: str):
 
 
 async def check_table(dsn: str, dbname: str, tbname: str):
-    async with asyncpg.connect(dsn, database=dbname) as conn:
+    async with await asyncpg.connect(dsn, database=dbname) as conn:
         exists = await conn.fetchval(
             "SELECT 1 FROM pg_tables WHERE tablename = $1",
             tbname
